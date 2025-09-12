675c45d246c28e1afa4d69fabc5de15443ef5f26
abhishek2446 <abhisheks200426@gmail.com>
2025-09-12 06:55:02 +0530

    Update Navigation.tsx

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -24,10 +24,10 @@ const Navigation = () => {
     <nav className="bg-white border-b border-border sticky top-0 z-50">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
-          {/* Logo */}
+          <img src="https://ik.imagekit.io/h2lupu11gm/logo1.jpg?updatedAt=1757639872494"></img>
           <Link to="/" className="flex items-center space-x-2">
             <BookOpen className="h-8 w-8 text-primary" />
-            <span className="text-2xl font-bold text-primary">Jairozon</span>
+            <span className="text-2xl font-bold text-primary"></span>
           </Link>
 
           {/* Desktop Search Bar */}
